b691a2ea1b741ec944e035d5286d142ca7e85080
WIN-40UH7PUH493\traininguser <ota.sato@skywill.jp>
2026-06-01 16:48:15 +0900

    repository作成完了

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -1,6 +1,10 @@
 package com.example.backend.repository;
 
-//userテーブルへのアクセスを担当するリポジトリ
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.backend.entity.User;
 
+//userテーブルへのアクセスを担当するリポジトリ
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
 }
