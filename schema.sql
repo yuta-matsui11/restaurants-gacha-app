e382534e677d9863e416e52c3c4d5f3164a2e3bf
WIN-SAR3TK13F3I\traininguser <kiraki.shibata@skywill.jp>
2026-06-01 15:42:50 +0900

    ガチャDB

---
+++ schema.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE restaurant_gacha_db;
+
+CREATE TABLE genres (
+    genre_id VARCHAR(50) PRIMARY KEY,
+    genre_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE users (
+    user_id BIGSERIAL PRIMARY KEY,
+    user_name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    nearest_station VARCHAR(100),
+    favorite_genre_id VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_users_genre
+        FOREIGN KEY (favorite_genre_id)
+        REFERENCES genres(genre_id)
+);
+
+CREATE TABLE histories (
+    history_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    station_name VARCHAR(100) NOT NULL,
+    genre_name VARCHAR(50),
+    restaurant_id VARCHAR(50) NOT NULL,
+    gacha_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_histories_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+    CONSTRAINT fk_histories_genre
+        FOREIGN KEY (genre_id)
+        REFERENCES genres(genre_id)
+);
+
+CREATE TABLE favorites (
+    favorite_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    restaurant_id VARCHAR(50) NOT NULL,
+
+    CONSTRAINT uq_favorites
+        UNIQUE (user_id, restaurant_id),
+
+    CONSTRAINT fk_favorites_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+);
