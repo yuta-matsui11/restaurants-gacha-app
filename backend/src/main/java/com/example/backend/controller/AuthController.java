1f8f6475c13e409694a486f355071f769b620e22
あなたの名前 <example@example.com>
2026-06-10 10:03:51 +0900

    genreINSERT追加

--- backend/src/main/java/com/example/backend/controller/AuthController.java
+++ backend/src/main/java/com/example/backend/controller/AuthController.java
@@ -18,6 +18,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class AuthController {
     @Autowired
     private AuthService authService;
